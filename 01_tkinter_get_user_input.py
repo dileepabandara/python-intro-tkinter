01cf79c8c481ef45c646b56cf5fcbf6bfdacadc5
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-01-14 21:20:30 +0530

    Add files

---
+++ 01_tkinter_get_user_input.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from functools import partial
+
+
+def validateLogin(username, password):
+    print("username entered :", username.get())
+    print("password entered :", password.get())
+    return
+
+def center(win):
+    win.update_idletasks()
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    win.deiconify()
+
+tkWindow = Tk()
+tkWindow.geometry('400x400')
+center(tkWindow)
+tkWindow.title('Tkinter Get User Input Form')
+
+usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)
+
+passwordLabel = Label(tkWindow, text="Password").grid(row=1, column=0)
+password = StringVar()
+passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+
+validateLogin = partial(validateLogin, username, password)
+
+loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)
+
+tkWindow.mainloop()
---
+++ 02_tkinter_simple_login_form.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import tkinter.messagebox as tm
+
+
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_username = Label(self, text="Username")
+        self.label_password = Label(self, text="Password")
+
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=1, sticky=E)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
+
+        self.checkbox = Checkbutton(self, text="Keep me logged in")
+        self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        print("Clicked")
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        print(username, password)
+
+        if username == "12" and password == "12" or username == "23" and password == "23":
+            welcomeMessage = "Welcome", username
+            tm.showinfo("Login info", welcomeMessage)
+        else:
+            tm.showerror("Login error", "Incorrect username")
+
+def center(win):
+    win.update_idletasks()
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    win.deiconify()
+
+root = Tk()
+root.geometry('400x400')
+root.title('Tkinter Simple Login Form')
+center(root)
+lf = LoginFrame(root)
+root.mainloop()
